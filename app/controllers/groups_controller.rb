a81e3eed009619e9419343a32ba8cdd10e6c9fd8
Hidetaka <hidetaka.48691001@icloud.com>
2015-10-29 12:58:08 +0900

    make chat

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,13 @@
 class GroupsController < ApplicationController
   def create
-    @group = Group.create
-    @group.groups_users.create(user_id: params[:user_id])
-    @group.groups_users.create(user: current_user)
+    group = current_user.same_group_with(User.find(params[:user_id]))
+    if group
+      @group = group
+    else
+      @group = Group.create
+      @group.groups_users.create(user_id: params[:user_id])
+      @group.groups_users.create(user: current_user)
+    end
     redirect_to [ @group, :messages ]
   end
 
